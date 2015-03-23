4929b1845f70667e26dcd0bee81ee4e4d6ca66d8
Alex Martin <martin.alex.t+github@gmail.com>
2015-03-22 23:41:04 -0400

    Initial Seed

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,8 @@
+class ArticlesController < ApplicationController
+  
+  def new
+    # @location = { :name => "WalMart", :description => "Nice place, dark parking lot.", :features => ['24hr', 'power', 'seating'], :loc => {:lat => "-34", :long => "82"}}
+    # render json: @location
+    
+  end
+end
