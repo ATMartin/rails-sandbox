e4dfad0768591953061e58ad89aa0747bd9ed30d
Alex Martin <martin.alex.t+github@gmail.com>
2015-03-23 00:56:56 -0400

    Add echo test route to Rails

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,18 @@
 class ArticlesController < ApplicationController
   
   def new
-    @location = { :name => "WalMart", :description => "Nice place, dark parking lot.", :features => ['24hr', 'power', 'seating'], :loc => {:lat => "-34", :long => "82"}}
-    render json: @location
+    # @location = { :name => "WalMart", :description => "Nice place, dark parking lot.", :features => ['24hr', 'power', 'seating'], :loc => {:lat => "-34", :long => "82"}}
+    # render json: @location
+    
+  end
+
+  def create
+    print params
+    render plain: params[:article].inspect
+  end
+
+  def echo
+    print params
+    render plain: params.inspect
   end
 end
