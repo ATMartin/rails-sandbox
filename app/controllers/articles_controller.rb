a4ca2478e29db395be25c87aac5287656e78af6c
Alex Martin <martin.alex.t+github@gmail.com>
2015-03-22 23:48:40 -0400

    JSON route test

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,7 @@
 class ArticlesController < ApplicationController
   
   def new
-    # @location = { :name => "WalMart", :description => "Nice place, dark parking lot.", :features => ['24hr', 'power', 'seating'], :loc => {:lat => "-34", :long => "82"}}
-    # render json: @location
-    
+    @location = { :name => "WalMart", :description => "Nice place, dark parking lot.", :features => ['24hr', 'power', 'seating'], :loc => {:lat => "-34", :long => "82"}}
+    render json: @location
   end
 end
